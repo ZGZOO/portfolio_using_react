086678394f7c85d745285aedd64fc584bbe24211
Zhijie Xu <jennyxu000@gmail.com>
2020-08-24 13:25:36 -0700

    skill set ok

--- portfolio/src/Components/About/Skills/Skills.js
+++ portfolio/src/Components/About/Skills/Skills.js
@@ -1,50 +1,76 @@
 import React from "react";
-import "./Skills.css";
+import "./Skills.scss";
 
 export default function Skills() {
   return (
     <div className="skills">
-      <p className="skills_title">Skills</p>
+      <p className="skills_title">Skill Set</p>
       <div className="skills_container">
         <div className="html">
-          <img alt="" />
           <p>HTML</p>
         </div>
         <div className="css">
-          <img alt="" />
           <p>CSS</p>
         </div>
+        <div className="scss">
+          <p>SCSS</p>
+        </div>
+        <div className="bootstrap">
+          <p>Bootstrap</p>
+        </div>
         <div className="javascript">
-          <img alt="" />
           <p>Javascript</p>
         </div>
+        <div className="jquery">
+          <p>jQuery</p>
+        </div>
         <div className="react">
-          <img alt="" />
           <p>React</p>
         </div>
         <div className="react-router">
-          <img alt="" />
           <p>React Router</p>
         </div>
-        <div className="jquery">
-          <img alt="" />
-          <p>jQuery</p>
+        <div className="node">
+          <p>Node.js</p>
         </div>
-        <div className="sass">
-          <img alt="" />
-          <p>Sass</p>
+        <div className="express">
+          <p>Express.js</p>
+        </div>
+        <div className="mongodb">
+          <p>MongoDB</p>
+        </div>
+        <div className="mongoose">
+          <p>mongoose</p>
+        </div>
+        <div className="postman">
+          <p>Postman</p>
+        </div>
+        <div className="mongodbAtlas">
+          <p>MongoDB Atlas</p>
+        </div>
+        <div className="heroku">
+          <p>Heroku</p>
+        </div>
+        <div className="github">
+          <p>GitHub</p>
+        </div>
+        <div className="ruby">
+          <p>Ruby</p>
+        </div>
+        <div className="rails">
+          <p>Rails</p>
+        </div>
+        <div className="postgresql">
+          <p>PostgreSQL</p>
         </div>
         <div className="photoshop">
-          <img alt="" />
           <p>Adobe Photoshop</p>
         </div>
-        <div className="adobe">
-          <img alt="" />
-          <p>Adobe XD</p>
-        </div>
         <div className="illustrator">
-          <img alt="" />
           <p>Adobe Illustrator</p>
+        </div>
+        <div className="adobe">
+          <p>Adobe XD</p>
         </div>
       </div>
     </div>
