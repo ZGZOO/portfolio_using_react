0ea74d5a77b6d40b4109f70edcca2267c69d6b3a
Zhijie Xu <jennyxu000@gmail.com>
2020-08-24 16:58:44 -0700

    deploy bug fix

--- portfolio/src/Components/Resume/Resume.jsx
+++ portfolio/src/Components/Resume/Resume.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Resume.scss";
 
 const Resume = () => {
   return (
-    <>
+    <div className="resume">
       <div>Hello Resume here</div>
-    </>
+    </div>
   );
 };
 
