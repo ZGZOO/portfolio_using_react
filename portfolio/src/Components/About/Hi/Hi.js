c6d16133f8883caa550ed745655e1c9b7f61e3ae
Zhijie Xu <jennyxu000@gmail.com>
2020-08-24 12:45:02 -0700

    added words to hi

--- portfolio/src/Components/About/Hi/Hi.js
+++ portfolio/src/Components/About/Hi/Hi.js
@@ -1,15 +1,31 @@
 import React from "react";
-import "./Hi.css";
+import "./Hi.scss";
 
 export default function Hi() {
   return (
     <div className="hi">
-      <span className="hey">Hi, I am Jenny. </span>
+      <span className="nameAndTitle">
+        Hi, I am Jenny. <br />
+        <br />A Full-Stack Web Developer.
+      </span>
       <br></br>
       <br></br>
-      <span className="aboutwords">
-        I am a web developer and am currently learning more skills at General Assembly.
-        I love creating beautiful websites.
+      <span className="aboutMe">
+        I have a passion for web development, and enjoy building various web
+        applications. <br />
+        <br />
+        Currently, I am a CS senior at San José State University. <br />
+        <br />
+        While learning fundamentals at school, I am constantly expanding my
+        knowledge in my free time. <br />
+        <br />
+        This past summer, I took Software Engineering Immersive Online bootcamp
+        at General Assembly. From there, I learned Full-Stack web development,
+        and I love it! <br />
+        <br />
+        Below, you will see my skill set and my projects.
+        <br />
+        <br />
       </span>
     </div>
   );
