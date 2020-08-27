350aaecb19e329631c06ba9bdaf4a140fa2344cb
Zhijie Xu <jennyxu000@gmail.com>
2020-08-27 13:53:41 -0700

    add bottom contacts

--- portfolio/src/Components/Resume/Resume.jsx
+++ portfolio/src/Components/Resume/Resume.jsx
@@ -3,13 +3,35 @@ import "./Resume.scss";
 
 const Resume = () => {
   return (
-    <div className="resume" id="resume">
-      <div>Hello Resume here</div>
-      <iframe
-        src="https://drive.google.com/file/d/1B1Xla7YkJvmshLpAb29R_psOuDRXWiWa/view?usp=sharing"
-        className="resumepdf"
-      ></iframe>
-    </div>
+    <>
+      <div className="resume" id="resume">
+        <div>Hello Resume here</div>
+        <iframe
+          src="https://drive.google.com/file/d/1B1Xla7YkJvmshLpAb29R_psOuDRXWiWa/view?usp=sharing"
+          className="resumepdf"
+        ></iframe>
+      </div>
+
+      <div className="socialmedia">
+        <a className="github icons" href="https://github.com/ZGZOO" target="_blank">
+          <i class="fab fa-github mediaIcon githubIcon"></i>github
+        </a>
+        <a
+          className="linkedin icons"
+          href="https://www.linkedin.com/in/jennyzhijiexu/"
+          target="_blank"
+        >
+          <i class="fab fa-linkedin mediaIcon linkedinIcon"></i>linkedin
+        </a>
+        <a
+          className="email icons"
+          href="mailto: jennyxu000@gmail.com"
+          target="_blank"
+        >
+          <i class="fas fa-paper-plane mediaIcon emailIcon"></i>email
+        </a>
+      </div>
+    </>
   );
 };
 
